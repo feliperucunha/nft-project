f195870c2fa3420b502d9491ca5e29a5e633bd8b
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-04-25 12:22:58 -0300

    nft page completed with mocked data

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -21,8 +21,40 @@ function NFTDropPage() {
         </div>
       </div>
 
-      <div>
+      <div className="flex flex-1 flex-col p-12 lg:col-span-6">
+        <header className="flex items-center justify-between">
+          <h1 className="w-52 cursor-pointer text-xl font-extralight sm:w-96">
+            The{' '}
+            <span className="font-extrabold underline decoration-pink-600/50">
+              New Age Apes
+            </span>{' '}
+            NFT Market Place
+          </h1>
 
+          <button className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-2.5 lg:text-base">
+            Sign In
+          </button>
+        </header>
+
+        <hr className="my-2 border" />
+
+        <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
+          <img
+            className="w-80 object-cover pb-10 lg:h-40"
+            src="/images/manymonke.jpeg"
+            alt="NFT Monkey Grid"
+          />
+
+          <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
+            The New Age Apes Coding Club | NFT Drop
+          </h1>
+
+          <p className="pt-2 text-xl text-green-500">13 / 21 NTF's claimed</p>
+        </div>
+
+        <button className="mt-10 h-16 w-full rounded-full bg-red-600 text-white">
+          Mint NFT (0.01 ETH)
+        </button>
       </div>
     </div>
   )
